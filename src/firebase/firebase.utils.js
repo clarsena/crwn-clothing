038a47106287937420f00b1eacdc42b0eb9059d5
Christopher Arsenault <carsenault@thekennagroup.com>
2019-10-31 16:34:59 -0400

    in the process of firebase authentication - video 12

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,23 +3,29 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBdpbwOTK2I8NY_Ntc4CzxAAeaK923gmSI",
-    authDomain: "crwn-db-80e82.firebaseapp.com",
-    databaseURL: "https://crwn-db-80e82.firebaseio.com",
-    projectId: "crwn-db-80e82",
-    storageBucket: "crwn-db-80e82.appspot.com",
-    messagingSenderId: "821604616189",
-    appId: "1:821604616189:web:e679ab437d1e1a3774bb94",
-    measurementId: "G-D8MVPF5DLC"
-  };
+		apiKey: "AIzaSyBdpbwOTK2I8NY_Ntc4CzxAAeaK923gmSI",
+		authDomain: "crwn-db-80e82.firebaseapp.com",
+		databaseURL: "https://crwn-db-80e82.firebaseio.com",
+		projectId: "crwn-db-80e82",
+		storageBucket: "crwn-db-80e82.appspot.com",
+		messagingSenderId: "821604616189",
+		appId: "1:821604616189:web:e679ab437d1e1a3774bb94",
+		measurementId: "G-D8MVPF5DLC"
+	};
 
-  firebase.initializeApp(config);
+	export const createUserProfileDocument = async (userAuth, additionalData) => {
+		if(!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+		console.log(firestore.doc('users/2183698ht9'))
+	}
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+	firebase.initializeApp(config);
 
-  export default firebase;
+	export const auth = firebase.auth();
+	export const firestore = firebase.firestore();
+
+	const provider = new firebase.auth.GoogleAuthProvider();
+	provider.setCustomParameters({ prompt: 'select_account' });
+	export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+	export default firebase;
