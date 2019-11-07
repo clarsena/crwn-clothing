568c45932a9fb75f66da3edd4cb90a95319d1d4b
Christopher Arsenault <carsenault@thekennagroup.com>
2019-11-07 16:59:07 -0500

    added styled components to homepage, header and custom buttons

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,7 +19,6 @@ const config = {
 		const userRef = firestore.doc(`users/${userAuth.uid}`);
 		const snapShot = await userRef.get();
 
-		console.log(snapShot)
 		if(!snapShot.exists) {
 			const { displayName, email } = userAuth;
 			const createdAt = new Date();
