b7b7dea7d71204ba02cc4422fad8398b762b2477
Christopher Arsenault <carsenault@thekennagroup.com>
2019-11-20 16:18:06 -0500

    updated our application App component, sign-in component and sign-up component to use hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,27 +6,28 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import {
-  CollectionPageContainer,
-  CollectionTitle,
-  CollectionPageItemsContainer
+	CollectionPageContainer,
+	CollectionTitle,
+	CollectionPageItemsContainer
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
-  return (
-    <CollectionPageContainer>
-      <CollectionTitle>{title}</CollectionTitle>
-      <CollectionPageItemsContainer>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </CollectionPageItemsContainer>
-    </CollectionPageContainer>
-  );
-};
+
+	const { title, items } = collection;
+	return (
+		<CollectionPageContainer>
+		<CollectionTitle>{title}</CollectionTitle>
+		<CollectionPageItemsContainer>
+		{items.map(item => (
+		<CollectionItem key={item.id} item={item} />
+		))}
+		</CollectionPageItemsContainer>
+		</CollectionPageContainer>
+	);
+	};
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
+	collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
 export default connect(mapStateToProps)(CollectionPage);
